2cb3663d0a2d47a891bfd0d726d61a1893073274
Jay Wikstrom <jaywikstrom@yahoo.com>
2021-08-26 19:33:15 -0500

    Route for add movie with inputs, buttons textarea, and dropdown

---
+++ src/components/AddMovie/AddMovie.jsx
@@ -0,0 +1,53 @@
+import { useHistory } from 'react-router-dom';
+
+function AddMovie() {
+    const history = useHistory();
+
+    const handleCancelClick = () => {
+        console.log('clicked')
+        history.push('/home');
+    }
+
+    const handleSaveClick = () => {
+        console.log('clicked')
+        history.push('/home');
+    }
+
+    return (
+        <div>
+            <input 
+                type="text"
+                placeholder="Movie Title"
+            />
+            <input
+                type="text"
+                placeholder="Movie Title"
+                placeholder="Movie Poster"
+            />
+            <textarea></textarea>
+
+            {/* <label htmlFor="genres">Choose a genre:</label> */}
+
+            <select name="cars" id="cars">
+                <option value="adventure">Adventure</option>
+                <option value="animated">Animated</option>
+                <option value="biographical">Biographical</option>
+                <option value="comedy">Comedy</option>
+                <option value="diaster">Diaster</option>
+                <option value="drama">Drama</option>
+                <option value="epic">Epic</option>
+                <option value="fantasy">Fantasy</option>
+                <option value="musical">Musical</option>
+                <option value="romantic">Romantic</option>
+                <option value="scienceFiction">Science Fiction</option>
+                <option value="spaceOpera">Space-Opera</option>
+                <option value="superhero">Superhero</option>
+            </select>
+            <br />
+            <br />
+            <button onClick={handleSaveClick}>Save</button>
+            <button onClick={handleCancelClick}>Cancel</button>
+        </div>
+    )
+}
+export default AddMovie;
--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,6 +1,8 @@
 import {HashRouter as Router, Route} from 'react-router-dom';
 import './App.css';
-import MovieList from '../MovieList/MovieList'
+import MovieList from '../MovieList/MovieList';
+import DisplayDetails from '../Details/Details';
+import AddMovie from '../AddMovie/AddMovie';
 
 function App() {
   return (
@@ -10,13 +12,21 @@ function App() {
         <Route path="/" exact>
           <MovieList />
         </Route>
+        <Route path="/home" exact>
+          <MovieList />
+        </Route>
         
         {/* Details page */}
         <Route path="/details" exact>
-          <h1>This is the details page</h1>
+          <DisplayDetails />
         </Route>
 
         {/* Add Movie page */}
+        <Route path="/add-movie" exact>
+          <AddMovie />
+        </Route>
+
+        
       </Router>
     </div>
   );
