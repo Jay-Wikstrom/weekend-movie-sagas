8d554cd76e9205d94899ef572aeda9e2ccb74355
Jay Wikstrom <jaywikstrom@yahoo.com>
2021-08-28 19:37:54 -0500

    Add form to add movie

--- src/components/AddMovie/AddMovie.jsx
+++ src/components/AddMovie/AddMovie.jsx
@@ -15,38 +15,40 @@ function AddMovie() {
 
     return (
         <div>
-            <input
-                type="text"
-                placeholder="Movie Title"
-            />
-            <input
-                type="text"
-                placeholder="Movie Title"
-                placeholder="Movie Poster"
-            />
-            <textarea></textarea>
+            <form onSubmit={handleSaveClick}>
+                <input
+                    type="text"
+                    placeholder="Movie Title"
+                />
+                <input
+                    type="text"
+                    placeholder="Movie Title"
+                    placeholder="Movie Poster"
+                />
+                <textarea></textarea>
 
-            {/* <label htmlFor="genres">Choose a genre:</label> */}
+                {/* <label htmlFor="genres">Choose a genre:</label> */}
 
-            <select name="cars" id="cars">
-                <option value="adventure">Adventure</option>
-                <option value="animated">Animated</option>
-                <option value="biographical">Biographical</option>
-                <option value="comedy">Comedy</option>
-                <option value="diaster">Diaster</option>
-                <option value="drama">Drama</option>
-                <option value="epic">Epic</option>
-                <option value="fantasy">Fantasy</option>
-                <option value="musical">Musical</option>
-                <option value="romantic">Romantic</option>
-                <option value="scienceFiction">Science Fiction</option>
-                <option value="spaceOpera">Space-Opera</option>
-                <option value="superhero">Superhero</option>
-            </select>
-            <br />
-            <br />
-            <button onClick={handleSaveClick}>Save</button>
-            <button onClick={handleCancelClick}>Cancel</button>
+                <select name="cars" id="cars">
+                    <option value="adventure">Adventure</option>
+                    <option value="animated">Animated</option>
+                    <option value="biographical">Biographical</option>
+                    <option value="comedy">Comedy</option>
+                    <option value="diaster">Diaster</option>
+                    <option value="drama">Drama</option>
+                    <option value="epic">Epic</option>
+                    <option value="fantasy">Fantasy</option>
+                    <option value="musical">Musical</option>
+                    <option value="romantic">Romantic</option>
+                    <option value="scienceFiction">Science Fiction</option>
+                    <option value="spaceOpera">Space-Opera</option>
+                    <option value="superhero">Superhero</option>
+                </select>
+                <br />
+                <br />
+                <button>Save</button>
+                <button onClick={handleCancelClick}>Cancel</button>
+            </form>
         </div>
     )
 }
